eed84e789e0a72e79f50b725cd8ef3f1c3d66f18
Eivlys <vamcheryl@gmail.com>
2024-05-31 14:28:52 +0800

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './styles/output.css'
 import './styles/main.css'
 import type { Metadata } from 'next'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-0RP724T7RQ" />
     </html>
   )
 }
